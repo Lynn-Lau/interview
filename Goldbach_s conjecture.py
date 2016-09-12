b1eb5c349bedc3b54f41d82fc58df07a39d8e764
Lynn <liu.silin@foxmail.com>
2016-09-12 11:28:09 +0800

    Goldbach's Conjecture

---
+++ Goldbach_s conjecture.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+# Goldbach's Conjecture from Tencent's Interview
+# Author:Lynn Lau
+# Date: 2016/09/12
+'''
+任何一个大于2的正整数都可以写成两个质数之和,
+通过Python验证这一猜想。
+'''
+
+import math
+
+
+def isprime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(math.sqrt(num))+1):
+        if num%i == 0:
+            return False
+    return True
+
+primelist = list()
+for j in range(2, 1000):
+    if isprime(j):
+        primelist.append(j)
+# print primelist
+
+def sum(data):
+    count = 0
+    for m in range(0, len(primelist)):
+        for n in range(m, len(primelist)):
+            if primelist[m] + primelist[n] == data:
+                count = count + 1
+
+    return count
+
+result = sum(int(raw_input()))
+print result
